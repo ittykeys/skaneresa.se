c6d1c29f16d99f99ec88c52c201143b196f5dce9
ittykeys <peter@peternilsson.info>
2024-04-02 04:46:34 +0200

    Comments

--- js/notification_en.js
+++ js/notification_en.js
@@ -3,7 +3,7 @@
  * Author: Peter Nilsson (@ittykeys)
  * Date: April 2, 2024
  * License: © 2024 Peter Nilsson, released under the GPLv3 License. See LICENSE file for details.
- * Description: Main js file for site
+ * Description: Language vars English
  */
 
 var notificationText = "Both from and to fields are required";
