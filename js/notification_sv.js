7a78fb97c7e1d73c83c530d5701c54280651e200
ittykeys <peter@peternilsson.info>
2024-06-10 18:28:11 +0200

    Notice

--- js/notification_sv.js
+++ js/notification_sv.js
@@ -3,7 +3,7 @@
  * Author: Peter Nilsson (@ittykeys)
  * Date: April 2, 2024
  * License: © 2024 Peter Nilsson, released under the GPLv3 License. See LICENSE file for details.
- * Description: Language vars English
+ * Description: Language vars Swedish
  */
 
 var notificationText = "Både från och till-fält krävs";
